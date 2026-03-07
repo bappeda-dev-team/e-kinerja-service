ca66da0f3512d635c6c516bbdbe346bc112b7178
Daniel <danielfebrian61@gmail.com>
2026-03-08 04:20:37 +0700

    added runMigrate() script in main.go

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"aplikasi-internal/config"
 	"aplikasi-internal/internal/helpers"
 	"aplikasi-internal/routes"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -20,6 +25,8 @@ import (
 
 func main() {
 	godotenv.Load()
+
+	runMigrations()
 
 	config.ConnectDB()
 
@@ -48,4 +55,27 @@ func main() {
 	routes.SetupRoutes(e)
 
 	e.Logger.Fatal(e.Start(":" + os.Getenv("APP_PORT")))
+}
+
+func runMigrations() {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+
+	m, err := migrate.New("file://migrations", dsn)
+	if err != nil {
+		log.Fatal("Migration init failed:", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("Migration failed:", err)
+	}
+
+	log.Println("✅ Migration sukses")
 }
