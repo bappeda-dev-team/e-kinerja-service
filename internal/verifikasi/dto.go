c79f82f24ab01cadda9af41a3e8bd8d6d60b50d5
zulfikar1609 <83656229+zulfikar1609@users.noreply.github.com>
2026-04-04 22:39:12 +0700

    getall verifikasi

--- internal/verifikasi/dto.go
+++ internal/verifikasi/dto.go
@@ -1,6 +1,9 @@
 package verifikasi
 
-import "time"
+import (
+	"aplikasi-internal/internal/permintaan"
+	"time"
+)
 
 // === Request ===
 
@@ -20,6 +23,30 @@ type VerifikasiResponse struct {
 	StatusVerified string    `json:"status_verified"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
+}
+
+type PemdaInfo struct {
+	ID			string     `json:"id"`
+	Name		string	   `json:"name"`
+	Logo		string 	   `json:"logo"`
+}
+
+type AplikasiInfo struct {
+	ID			string     `json:"id"`
+	Name		string	   `json:"name"`
+	Logo		string 	   `json:"logo"`
+}
+
+type PermintaanInfo struct {
+	ID                string                 `json:"id"`
+	Pemda             PemdaInfo              `json:"pemda"`
+	Aplikasi          AplikasiInfo           `json:"aplikasi"`
+	Menu              string                 `json:"menu"`
+	KondisiAwal       string                 `json:"kondisi_awal"`
+	KondisiDiharapkan string                 `json:"kondisi_diharapkan"`
+	TanggalPesanan    time.Time              `json:"tanggal_pesanan"`
+	TanggalDeadline   time.Time              `json:"tanggal_deadline"`
+	Lampiran          permintaan.StringArray `json:"lampiran"`
 }
 
 type ProgrammerInfo struct {
@@ -45,9 +72,10 @@ type VerifikatorInfo struct {
 
 type VerifikasiDetailResponse struct {
 	ID             string          `json:"id"`
+	Permintaan     PermintaanInfo  `json:"permintaan"`
 	Laporan        LaporanInfo     `json:"laporan"`
 	Verifikator    VerifikatorInfo `json:"verifikator"`
-	Komentar       string          `json:"komentar"`
+	Komentar       *string          `json:"komentar"`
 	StatusVerified string          `json:"status_verified"`
 	CreatedAt      time.Time       `json:"created_at"`
 	UpdatedAt      time.Time       `json:"updated_at"`
